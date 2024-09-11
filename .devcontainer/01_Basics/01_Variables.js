f4968f053efad26d4f0673ac83b28d4b421203b3
FaizanRizwee <149058121+FaizanRizwee@users.noreply.github.com>
2024-09-11 02:40:15 +0000

    Variables

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,19 @@
+const accountId = 1121
+let accountEmail = "faizan@google.com"
+var accountPass = "636633"
+accountCity = "Delhi" //Bina Variable Name k bhi Define kr skte h bt not preferred
+let accountState;
+
+// acccountId= 2  Not Allowed
+
+accountEmail = "faiz@gmail.com"
+accountPass = "66767"
+accountCity = "Patna"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
+
+/* {
+    curly braces are scopes
+ } */
