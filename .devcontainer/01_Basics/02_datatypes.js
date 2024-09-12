ddc56cefd5de8e4e97b3c6c12c39a33f882c59df
FaizanRizwee <149058121+FaizanRizwee@users.noreply.github.com>
2024-09-12 14:43:10 +0000

    datatypes

---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version(or JS). ab ye likhne ki bhi zarurat nhi hai.
+
+//alert(3+3) //error bcz we r in node not in browser in node we have diff. syntax
+
+let name = "Hitesh"
+let age = 19
+let isLoggedIn = false
+
+// number => may be 2 to the power 53 (range provided by mem addr)
+// bigint (for very big no. eg stock me trading me etc)
+//string => '' / ""
+// boolean => true/false
+
+// null => standalone value bhi h (representn of an empty variable) eg maine req ki temp chahiye aur temp nhi pta to 0 to bhej nhi skte bcz zero khud 1 temp hai isilie null bhejenge mtlb empty h temp abhi aaya hi nhi h)
+
+// undefined => value not assinged till now only variable defined
+
+// symbol => for finding uniquness
