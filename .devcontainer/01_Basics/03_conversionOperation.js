58983dcb58cae8bc02eecd60a7c33a701241387a
FaizanRizwee <149058121+FaizanRizwee@users.noreply.github.com>
2024-11-07 05:59:40 +0000

    operatons

--- .devcontainer/01_Basics/03_conversionOperation.js
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -38,35 +38,41 @@ let negValue = -value
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+// console.log(2**3);    Power 2^3
 // console.log(2/3);
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " hitesh"   //for space after merging so space in hitesh
 
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
+// complex situations
+
+// console.log("1" + 2);    "" k andr space denge tb space aaiga 
 // console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);   122 is case me sb as a string treat hoga
+// console.log(1 + 2 + "2");   32  isme phle jo opn hoga ho jaiga then string
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log("1" + (2 + 2)); 14   Use paranthesis () for better understanding
 
-// console.log(+true);
-// console.log(+"");
+// console.log( (3 + 4) * 5 % 3); 
+
+// .console.log(true);  true
+// console.log(+true); 1  but not best
+// console.log(+"");   0  but not best
+ 
+//Assignment Operator
 
 let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2 //also not good method
 
 let gameCounter = 100
-++gameCounter;
+// gameCounter++;  101
+++gameCounter;   //101
 console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
-
-
